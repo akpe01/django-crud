54c352d302d9d7910d6fc04fd5ce2461fbb8de0f
Akpe peace <53478505+akpe01@users.noreply.github.com>
2022-06-25 18:20:09 +0200

    Update views.py

--- I4G012348oZU/blog/views.py
+++ I4G012348oZU/blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views import generic
+from I4G012348oZU.blog.models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
+class PostListView(generic.ListView):
+    model = Post
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(generic.UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+
+
+
+
+
+
